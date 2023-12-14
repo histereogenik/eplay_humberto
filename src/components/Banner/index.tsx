a32ea90b6960c1677a7e58982b40bc3a0a883108
Humberto Nacif <gigantedeluz@gmail.com>
2023-12-13 22:45:56 -0300

    Creating Button component

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,6 +2,7 @@ import { Imagem, Titulo, Precos } from './styles'
 
 import bannerImg from '../../assets/images/banner-homem-aranha.png'
 import Tag from '../Tag'
+import Button from '../Button'
 
 const Banner = () => (
   <Imagem style={{ backgroundImage: `url(${bannerImg})` }}>
@@ -12,6 +13,13 @@ const Banner = () => (
         De <span>R$ 250,00</span> <br />
         por apenas R$ 99,90
       </Precos>
+      <Button
+        type="link"
+        to="/produto"
+        title="Clique aqui para aproveitar essa oferta"
+      >
+        Aproveitar
+      </Button>
     </div>
   </Imagem>
 )
