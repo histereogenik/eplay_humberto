2d44baaf1da13c9fc2ce6a38d434f37cb215162d
Humberto Nacif <gigantedeluz@gmail.com>
2023-12-14 22:16:02 -0300

    Making adjustments on the Product component

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,19 +1,35 @@
 import Tag from '../Tag'
 
-import { Card, Descricao, Titulo } from './styles'
+import { Card, Descricao, Titulo, Infos } from './styles'
 
-const Product = () => (
+type Props = {
+  title: string
+  category: string
+  system: string
+  description: string
+  infos: string[]
+  image: string
+}
+
+const Product = ({
+  category,
+  description,
+  image,
+  infos,
+  system,
+  title
+}: Props) => (
   <Card>
-    <img src="//placehold.it/222x250" />
-    <Titulo>Nome do Jogo</Titulo>
-    <Tag>Categoria</Tag>
-    <Tag>Windows</Tag>
-    <Descricao>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque cumque
-      ipsam necessitatibus maiores, ut dignissimos. Eveniet nesciunt molestias
-      deleniti aliquid. Cum, ut. Libero nisi hic architecto repudiandae officia
-      esse vero?
-    </Descricao>
+    <img src={image} alt={title} />
+    <Infos>
+      {infos.map((info) => (
+        <Tag key={info}>{info}</Tag>
+      ))}
+    </Infos>
+    <Titulo>{title}</Titulo>
+    <Tag>{category}</Tag>
+    <Tag>{system}</Tag>
+    <Descricao>{description}</Descricao>
   </Card>
 )
 
