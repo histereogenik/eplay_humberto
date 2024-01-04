7f55ff56f76c89437b06ba67c3a35bf31127cd39
Humberto Nacif <gigantedeluz@gmail.com>
2024-01-03 21:00:54 -0300

    Implementing Redux on the Banner component

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -5,15 +5,13 @@ import { Game } from '../../pages/Home'
 import Tag from '../Tag'
 import Button from '../Button'
 import { Imagem, Titulo, Precos } from './styles'
-import { formataPreco } from '../ProductsList'
-const Banner = () => {
-  const [game, setGame] = useState<Game>()
 
-  useEffect(() => {
-    fetch('https://fake-api-tau.vercel.app/api/eplay/destaque')
-      .then((res) => res.json())
-      .then((res) => setGame(res))
-  }, [])
+import { formataPreco } from '../ProductsList'
+
+import { useGetFeaturedGameQuery } from '../../services/api'
+
+const Banner = () => {
+  const { data: game, isLoading } = useGetFeaturedGameQuery()
 
   if (!game) {
     return <h3>Carregando...</h3>
