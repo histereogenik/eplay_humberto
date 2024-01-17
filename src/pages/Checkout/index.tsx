87fa7b2738262700ca0132394452e7b6a42bdeba
Humberto Nacif <gigantedeluz@gmail.com>
2024-01-16 22:05:57 -0300

    Creating card payment form on Checkout page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -43,6 +43,48 @@ const Checkout = () => (
           código de ativação do jogo adquirido ocorrerá somente após a aprovação
           do pagamento do boleto.
         </p>
+        <Row>
+          <InputGroup>
+            <label htmlFor="cardOwner">Nome do titular do cartão</label>
+            <input id="cardOwner" type="text" />
+          </InputGroup>
+          <InputGroup>
+            <label htmlFor="cpfCardOwner">CPF do titular do cartão</label>
+            <input id="cpfCardOwner" type="text" />
+          </InputGroup>
+        </Row>
+        <Row marginTop="24px">
+          <InputGroup>
+            <label htmlFor="cardDisplayName">Nome no cartão</label>
+            <input id="cardDisplayName" type="text" />
+          </InputGroup>
+          <InputGroup>
+            <label htmlFor="cardNumber">Número do cartão</label>
+            <input id="cardNumber" type="text" />
+          </InputGroup>
+          <InputGroup maxWidth="123px">
+            <label htmlFor="expiresMonth">Mês do vencimento</label>
+            <input id="expiresMonth" type="text" />
+          </InputGroup>
+          <InputGroup maxWidth="123px">
+            <label htmlFor="expiresYear">Ano de vencimento</label>
+            <input id="expiresYear" type="text" />
+          </InputGroup>
+          <InputGroup maxWidth="48px">
+            <label htmlFor="cardCode">CVV</label>
+            <input id="cardCode" type="text" />
+          </InputGroup>
+        </Row>
+        <Row marginTop="24px">
+          <InputGroup maxWidth="116px">
+            <label htmlFor="installments">Parcelamento</label>
+            <select>
+              <option>1x de R$ 200,00</option>
+              <option>2x de R$ 200,00</option>
+              <option>3x de R$ 200,00</option>
+            </select>
+          </InputGroup>
+        </Row>
       </div>
     </CheckoutCard>
     <Button type="button" title="Clique aqui para finalizar a compra">
