a890b34ba4bf725ce1da1a8afe5fc352be7b43df
Humberto Nacif <gigantedeluz@gmail.com>
2024-01-17 21:56:28 -0300

    Applying Formik to Checkout form

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { useFormik } from 'formik'
+
 import Button from '../../components/Button'
 import CheckoutCard from '../../components/Card'
 
@@ -10,33 +12,89 @@ import cartao from '../../assets/images/cartao.png'
 const Checkout = () => {
   const [payWithCard, setPayWithCard] = useState(false)
 
+  const form = useFormik({
+    initialValues: {
+      fullName: '',
+      email: '',
+      cpf: '',
+      deliveryEmail: '',
+      confirmDeliveryEmail: '',
+      cardOwner: '',
+      cpfCardOwner: '',
+      cardDisplayName: '',
+      cardNumber: '',
+      expiresMonth: '',
+      expiresYear: '',
+      cardCode: '',
+      installments: 1
+    },
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
+
   return (
-    <div className="container">
+    <form onSubmit={form.handleSubmit} className="container">
       <CheckoutCard title="Dados de cobrança">
         <>
           <Row>
             <InputGroup>
               <label htmlFor="fullName">Nome Completo</label>
-              <input id="fullName" type="text" />
+              <input
+                id="fullName"
+                type="text"
+                name="fullName"
+                onChange={form.handleChange}
+                onBlur={form.handleBlur}
+                value={form.values.fullName}
+              />
             </InputGroup>
             <InputGroup>
               <label htmlFor="email">E-mail</label>
-              <input id="email" type="email" />
+              <input
+                id="email"
+                type="email"
+                name="email"
+                onChange={form.handleChange}
+                onBlur={form.handleBlur}
+                value={form.values.email}
+              />
             </InputGroup>
             <InputGroup>
               <label htmlFor="cpf">CPF</label>
-              <input id="cpf" type="text" />
+              <input
+                id="cpf"
+                type="text"
+                name="cpf"
+                onChange={form.handleChange}
+                onBlur={form.handleBlur}
+                value={form.values.cpf}
+              />
             </InputGroup>
           </Row>
           <h3 className="margin-top">Dados de entrega - conteúdo digital</h3>
           <Row>
             <InputGroup>
               <label htmlFor="deliveryEmail">E-mail</label>
-              <input id="deliveryEmail" type="email" />
+              <input
+                id="deliveryEmail"
+                type="email"
+                name="deliveryEmail"
+                onChange={form.handleChange}
+                onBlur={form.handleBlur}
+                value={form.values.deliveryEmail}
+              />
             </InputGroup>
             <InputGroup>
               <label htmlFor="confirmDeliveryEmail">Confirme o E-mail</label>
-              <input id="confirmDeliveryEmail" type="email" />
+              <input
+                id="confirmDeliveryEmail"
+                type="email"
+                name="confirmDeliveryEmail"
+                onChange={form.handleChange}
+                onBlur={form.handleBlur}
+                value={form.values.confirmDeliveryEmail}
+              />
             </InputGroup>
           </Row>
         </>
@@ -63,41 +121,96 @@ const Checkout = () => {
                 <Row>
                   <InputGroup>
                     <label htmlFor="cardOwner">Nome do titular do cartão</label>
-                    <input id="cardOwner" type="text" />
+                    <input
+                      id="cardOwner"
+                      type="text"
+                      name="cardOwner"
+                      onChange={form.handleChange}
+                      onBlur={form.handleBlur}
+                      value={form.values.cardOwner}
+                    />
                   </InputGroup>
                   <InputGroup>
                     <label htmlFor="cpfCardOwner">
                       CPF do titular do cartão
                     </label>
-                    <input id="cpfCardOwner" type="text" />
+                    <input
+                      id="cpfCardOwner"
+                      type="text"
+                      name="cpfCardOwner"
+                      onChange={form.handleChange}
+                      onBlur={form.handleBlur}
+                      value={form.values.cpfCardOwner}
+                    />
                   </InputGroup>
                 </Row>
                 <Row marginTop="24px">
                   <InputGroup>
                     <label htmlFor="cardDisplayName">Nome no cartão</label>
-                    <input id="cardDisplayName" type="text" />
+                    <input
+                      id="cardDisplayName"
+                      type="text"
+                      name="cardDisplayName"
+                      onChange={form.handleChange}
+                      onBlur={form.handleBlur}
+                      value={form.values.cardDisplayName}
+                    />
                   </InputGroup>
                   <InputGroup>
                     <label htmlFor="cardNumber">Número do cartão</label>
-                    <input id="cardNumber" type="text" />
+                    <input
+                      id="cardNumber"
+                      type="text"
+                      name="cardNumber"
+                      onChange={form.handleChange}
+                      onBlur={form.handleBlur}
+                      value={form.values.cardNumber}
+                    />
                   </InputGroup>
                   <InputGroup maxWidth="123px">
                     <label htmlFor="expiresMonth">Mês do vencimento</label>
-                    <input id="expiresMonth" type="text" />
+                    <input
+                      id="expiresMonth"
+                      type="text"
+                      name="expiresMonth"
+                      onChange={form.handleChange}
+                      onBlur={form.handleBlur}
+                      value={form.values.expiresMonth}
+                    />
                   </InputGroup>
                   <InputGroup maxWidth="123px">
                     <label htmlFor="expiresYear">Ano de vencimento</label>
-                    <input id="expiresYear" type="text" />
+                    <input
+                      id="expiresYear"
+                      type="text"
+                      name="expiresYear"
+                      onChange={form.handleChange}
+                      onBlur={form.handleBlur}
+                      value={form.values.expiresYear}
+                    />
                   </InputGroup>
                   <InputGroup maxWidth="48px">
                     <label htmlFor="cardCode">CVV</label>
-                    <input id="cardCode" type="text" />
+                    <input
+                      id="cardCode"
+                      type="text"
+                      name="cardCode"
+                      onChange={form.handleChange}
+                      onBlur={form.handleBlur}
+                      value={form.values.cardCode}
+                    />
                   </InputGroup>
                 </Row>
                 <Row marginTop="24px">
                   <InputGroup maxWidth="150px">
                     <label htmlFor="installments">Parcelamento</label>
-                    <select>
+                    <select
+                      id="installments"
+                      name="installments"
+                      onChange={form.handleChange}
+                      onBlur={form.handleBlur}
+                      value={form.values.installments}
+                    >
                       <option>1x de R$ 200,00</option>
                       <option>2x de R$ 200,00</option>
                       <option>3x de R$ 200,00</option>
@@ -117,10 +230,14 @@ const Checkout = () => {
           </div>
         </>
       </CheckoutCard>
-      <Button type="button" title="Clique aqui para finalizar a compra">
+      <Button
+        onClick={form.handleSubmit}
+        type="button"
+        title="Clique aqui para finalizar a compra"
+      >
         Finalizar compra
       </Button>
-    </div>
+    </form>
   )
 }
 
