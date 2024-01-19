2817d87322fb328d9ec63f0368f06bbb0b1fb5c3
Humberto Nacif <gigantedeluz@gmail.com>
2024-01-18 22:16:44 -0300

    Adjusting links for the responsive design

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,18 @@
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import { open } from '../../store/reducers/cart'
 
-import { HeaderBar, LinkItem, Links, CartButton, Hamburger } from './styles'
+import {
+  HeaderBar,
+  LinkItem,
+  Links,
+  CartButton,
+  Hamburger,
+  HeaderRow,
+  NavMobile
+} from './styles'
 
 import logo from '../../assets/images/logo.svg'
 import carrinho from '../../assets/images/carrinho.svg'
@@ -11,8 +20,8 @@ import { RootReducer } from '../../store'
 
 const Header = () => {
   const dispatch = useDispatch()
-
   const { items } = useSelector((state: RootReducer) => state.cart)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const openCart = () => {
     dispatch(open())
@@ -20,34 +29,49 @@ const Header = () => {
 
   return (
     <HeaderBar>
-      <div>
-        <Hamburger>
-          <span />
-          <span />
-          <span />
-        </Hamburger>
-        <Link to="/">
-          <img src={logo} alt="EPLAY" />
-        </Link>
-        <nav>
-          <Links>
-            <LinkItem>
-              <Link to="/categories">Categorias</Link>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">Novidades</a>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">Promoções</a>
-            </LinkItem>
-          </Links>
-        </nav>
-      </div>
-      <CartButton onClick={openCart}>
-        {items.length}
-        <span> - produtos(s)</span>
-        <img src={carrinho} alt="carrinho" />
-      </CartButton>
+      <HeaderRow>
+        <div>
+          <Hamburger onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <span />
+            <span />
+            <span />
+          </Hamburger>
+          <Link to="/">
+            <img src={logo} alt="EPLAY" />
+          </Link>
+          <nav>
+            <Links>
+              <LinkItem>
+                <Link to="/categories">Categorias</Link>
+              </LinkItem>
+              <LinkItem>
+                <a href="#">Novidades</a>
+              </LinkItem>
+              <LinkItem>
+                <a href="#">Promoções</a>
+              </LinkItem>
+            </Links>
+          </nav>
+        </div>
+        <CartButton onClick={openCart}>
+          {items.length}
+          <span> - produtos(s)</span>
+          <img src={carrinho} alt="carrinho" />
+        </CartButton>
+      </HeaderRow>
+      <NavMobile className={isMenuOpen ? 'is-open' : ''}>
+        <Links>
+          <LinkItem>
+            <Link to="/categories">Categorias</Link>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Novidades</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Promoções</a>
+          </LinkItem>
+        </Links>
+      </NavMobile>
     </HeaderBar>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,21 +4,28 @@ import { breakpoints, cores } from '../../styles'
 export const Links = styled.ul`
   display: flex;
   margin-left: 40px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    margin-left: 0;
+    display: block;
+  }
 `
 export const HeaderBar = styled.header`
   background-color: ${cores.cinza};
   padding: 24px;
   border-radius: 16px;
   margin-bottom: 80px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
 
   a {
     color: ${cores.branca};
     text-decoration: none;
     font-weight: bold;
   }
+`
+export const HeaderRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
   > div {
     display: flex;
@@ -33,12 +40,26 @@ export const HeaderBar = styled.header`
       }
     }
   }
+`
+export const NavMobile = styled.nav`
+  display: none;
 
-  @media (max-width: ${breakpoints.tablet}) {
+  &.is-open {
+    display: block;
   }
 `
 export const LinkItem = styled.li`
   margin-right: 16px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    margin-right: 0;
+
+    a {
+      display: block;
+      padding: 16px;
+      text-align: center;
+    }
+  }
 `
 export const CartButton = styled.a`
   display: flex;
@@ -56,6 +77,7 @@ export const CartButton = styled.a`
 `
 export const Hamburger = styled.div`
   width: 32px;
+  cursor: pointer;
 
   span {
     height: 2px;
